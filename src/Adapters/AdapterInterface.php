4b915d1afce083ba69899baa3230994af663d314
MR <3058842-mrkr@users.noreply.gitlab.com>
2019-01-19 23:06:48 +0800

    Fix formatting

--- src/Adapters/AdapterInterface.php
+++ src/Adapters/AdapterInterface.php
@@ -8,8 +8,8 @@ interface AdapterInterface
     /**
      * Creates a new record in a table
      *
-     * @param  string $table Name of table to insert to
-     * @param  array  $data  Column-value pair of the data to insert
+     * @param string $table Name of table to insert to
+     * @param array $data Column-value pair of the data to insert
      * @return int The last insert id
      */
     public function create(string $table, array $data): int;
