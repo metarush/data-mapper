aa049c34da958b1849bbcd94b1551158dc81d0e2
zkwbbr <45949485+zkwbbr@users.noreply.github.com>
2019-01-22 02:51:57 +0800

    Add docs

--- src/Adapters/AdapterInterface.php
+++ src/Adapters/AdapterInterface.php
@@ -6,7 +6,7 @@ interface AdapterInterface
 {
 
     /**
-     * Creates a new record in a table
+     * Creates new record in a table
      *
      * @param string $table Name of table to insert to
      * @param array $data Column-value pair of the data to insert
@@ -14,11 +14,40 @@ interface AdapterInterface
      */
     public function create(string $table, array $data): int;
 
+    /**
+     * Find record in a table
+     *
+     * @param string $table Name of table to query
+     * @param array $where Column-value pair of the where clause
+     * @return array|null The record
+     */
     public function findOne(string $table, array $where): ?array;
 
+    /**
+     * Find all records in a table
+     *
+     * @param string $table Name of table to query
+     * @param array $where Column-value pair of the where clause
+     * @return array The records
+     */
     public function findAll(string $table, ?array $where): array;
 
+    /**
+     * Update record in a table
+     *
+     * @param string $table Name of table to update
+     * @param array $data Data to be updated
+     * @param array $where Column-value pair of the where clause
+     * @return void
+     */
     public function update(string $table, array $data, ?array $where): void;
 
+    /**
+     * Delete record in a table
+     *
+     * @param string $table Name of table where a record will be deleted
+     * @param array $where Column-value pair of the where clause
+     * @return void
+     */
     public function delete(string $table, ?array $where): void;
 }
